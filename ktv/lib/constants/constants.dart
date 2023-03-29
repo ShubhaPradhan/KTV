7f7ae9080dda238bd4ff87a1233e9b971e155eaa
shubha <shubahdon7@gmail.com>
2023-03-29 12:32:28 +0545

    color constants init

---
+++ ktv/lib/constants/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const domain = 'http://127.0.0.1:8000';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFF8A00);
+  static const Color red = Color(0xFFED1D1D);
+  static const Color white = Color(0xFFF5F5F5);
+  static const Color gray40 = Color(0xFFF1F1F1);
+  static const Color gray50 = Color(0xFFF0F0F0);
+  static const Color gray100 = Color(0xFFE0E0E0);
+  static const Color gray200 = Color(0xFFC2C2C2);
+  static const Color gray300 = Color(0xFFA3A3A3);
+  static const Color gray400 = Color(0xFF858585);
+  static const Color gray500 = Color(0xFF666666);
+  static const Color gray600 = Color(0xFF4D4D4D);
+  static const Color gray700 = Color(0xFF333333);
+  static const Color gray800 = Color(0xFF1A1A1A);
+  static const Color gray900 = Color(0xFF0A0A0A);
+}
